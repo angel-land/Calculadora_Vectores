fb8c26f74960ea9d3c5111388828146b7dbdfa83
angel-land <89943191+angel-land@users.noreply.github.com>
2021-09-05 13:41:36 -0300

    v0.1 Alpha
    Creacion de Repositorio

---
+++ Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace CalculdoraVectorial
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculadora));
+            this.VectorALbl = new System.Windows.Forms.Label();
+            this.VectorBLbl = new System.Windows.Forms.Label();
+            this.ITxt = new System.Windows.Forms.TextBox();
+            this.JTxt = new System.Windows.Forms.TextBox();
+            this.KTxt = new System.Windows.Forms.TextBox();
+            this.IITxt = new System.Windows.Forms.TextBox();
+            this.JJTxt = new System.Windows.Forms.TextBox();
+            this.KKTxt = new System.Windows.Forms.TextBox();
+            this.ILbl = new System.Windows.Forms.Label();
+            this.JLbl = new System.Windows.Forms.Label();
+            this.KLbl = new System.Windows.Forms.Label();
+            this.IILbl = new System.Windows.Forms.Label();
+            this.JJLbl = new System.Windows.Forms.Label();
+            this.KKLbl = new System.Windows.Forms.Label();
+            this.VecRLbl = new System.Windows.Forms.Label();
+            this.RVecLbl = new System.Windows.Forms.Label();
+            this.SumaBtn = new System.Windows.Forms.Button();
+            this.RestaBtn = new System.Windows.Forms.Button();
+            this.MultiplicacionBtn = new System.Windows.Forms.Button();
+            this.OtroBtn = new System.Windows.Forms.Button();
+            this.SalirBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // VectorALbl
+            // 
+            this.VectorALbl.AccessibleDescription = "";
+            this.VectorALbl.AutoSize = true;
+            this.VectorALbl.BackColor = System.Drawing.SystemColors.Window;
+            this.VectorALbl.Location = new System.Drawing.Point(12, 18);
+            this.VectorALbl.Name = "VectorALbl";
+            this.VectorALbl.Size = new System.Drawing.Size(92, 21);
+            this.VectorALbl.TabIndex = 0;
+            this.VectorALbl.Text = "Vector A:";
+            // 
+            // VectorBLbl
+            // 
+            this.VectorBLbl.AccessibleDescription = "";
+            this.VectorBLbl.AutoSize = true;
+            this.VectorBLbl.BackColor = System.Drawing.SystemColors.Window;
+            this.VectorBLbl.Location = new System.Drawing.Point(12, 70);
+            this.VectorBLbl.Name = "VectorBLbl";
+            this.VectorBLbl.Size = new System.Drawing.Size(91, 21);
+            this.VectorBLbl.TabIndex = 1;
+            this.VectorBLbl.Text = "Vector B:";
+            // 
+            // ITxt
+            // 
+            this.ITxt.Location = new System.Drawing.Point(110, 12);
+            this.ITxt.Name = "ITxt";
+            this.ITxt.Size = new System.Drawing.Size(41, 33);
+            this.ITxt.TabIndex = 2;
+            this.ITxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // JTxt
+            // 
+            this.JTxt.Location = new System.Drawing.Point(193, 12);
+            this.JTxt.Name = "JTxt";
+            this.JTxt.Size = new System.Drawing.Size(41, 33);
+            this.JTxt.TabIndex = 3;
+            this.JTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KTxt
+            // 
+            this.KTxt.Location = new System.Drawing.Point(269, 12);
+            this.KTxt.Name = "KTxt";
+            this.KTxt.Size = new System.Drawing.Size(41, 33);
+            this.KTxt.TabIndex = 4;
+            this.KTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // IITxt
+            // 
+            this.IITxt.Location = new System.Drawing.Point(110, 67);
+            this.IITxt.Name = "IITxt";
+            this.IITxt.Size = new System.Drawing.Size(41, 33);
+            this.IITxt.TabIndex = 5;
+            this.IITxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // JJTxt
+            // 
+            this.JJTxt.Location = new System.Drawing.Point(193, 67);
+            this.JJTxt.Name = "JJTxt";
+            this.JJTxt.Size = new System.Drawing.Size(41, 33);
+            this.JJTxt.TabIndex = 6;
+            this.JJTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KKTxt
+            // 
+            this.KKTxt.Location = new System.Drawing.Point(269, 66);
+            this.KKTxt.Name = "KKTxt";
+            this.KKTxt.Size = new System.Drawing.Size(41, 33);
+            this.KKTxt.TabIndex = 7;
+            this.KKTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ILbl
+            // 
+            this.ILbl.AutoSize = true;
+            this.ILbl.BackColor = System.Drawing.Color.Transparent;
+            this.ILbl.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ILbl.Location = new System.Drawing.Point(157, 11);
+            this.ILbl.Name = "ILbl";
+            this.ILbl.Size = new System.Drawing.Size(23, 34);
+            this.ILbl.TabIndex = 8;
+            this.ILbl.Text = "i";
+            // 
+            // JLbl
+            // 
+            this.JLbl.AutoSize = true;
+            this.JLbl.BackColor = System.Drawing.Color.Transparent;
+            this.JLbl.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.JLbl.Location = new System.Drawing.Point(240, 12);
+            this.JLbl.Name = "JLbl";
+            this.JLbl.Size = new System.Drawing.Size(23, 34);
+            this.JLbl.TabIndex = 9;
+            this.JLbl.Text = "j";
+            // 
+            // KLbl
+            // 
+            this.KLbl.AutoSize = true;
+            this.KLbl.BackColor = System.Drawing.Color.Transparent;
+            this.KLbl.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KLbl.Location = new System.Drawing.Point(316, 12);
+            this.KLbl.Name = "KLbl";
+            this.KLbl.Size = new System.Drawing.Size(29, 34);
+            this.KLbl.TabIndex = 10;
+            this.KLbl.Text = "k";
+            // 
+            // IILbl
+            // 
+            this.IILbl.AutoSize = true;
+            this.IILbl.BackColor = System.Drawing.Color.Transparent;
+            this.IILbl.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IILbl.Location = new System.Drawing.Point(157, 66);
+            this.IILbl.Name = "IILbl";
+            this.IILbl.Size = new System.Drawing.Size(23, 34);
+            this.IILbl.TabIndex = 11;
+            this.IILbl.Text = "i";
+            // 
+            // JJLbl
+            // 
+            this.JJLbl.AutoSize = true;
+            this.JJLbl.BackColor = System.Drawing.Color.Transparent;
+            this.JJLbl.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.JJLbl.Location = new System.Drawing.Point(240, 66);
+            this.JJLbl.Name = "JJLbl";
+            this.JJLbl.Size = new System.Drawing.Size(23, 34);
+            this.JJLbl.TabIndex = 12;
+            this.JJLbl.Text = "j";
+            // 
+            // KKLbl
+            // 
+            this.KKLbl.AutoSize = true;
+            this.KKLbl.BackColor = System.Drawing.Color.Transparent;
+            this.KKLbl.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KKLbl.Location = new System.Drawing.Point(316, 65);
+            this.KKLbl.Name = "KKLbl";
+            this.KKLbl.Size = new System.Drawing.Size(29, 34);
+            this.KKLbl.TabIndex = 13;
+            this.KKLbl.Text = "k";
+            // 
+            // VecRLbl
+            // 
+            this.VecRLbl.AutoSize = true;
+            this.VecRLbl.BackColor = System.Drawing.Color.Transparent;
+            this.VecRLbl.Location = new System.Drawing.Point(12, 130);
+            this.VecRLbl.Name = "VecRLbl";
+            this.VecRLbl.Size = new System.Drawing.Size(194, 21);
+            this.VecRLbl.TabIndex = 14;
+            this.VecRLbl.Text = "Vector Resultante R=";
+            // 
+            // RVecLbl
+            // 
+            this.RVecLbl.AutoSize = true;
+            this.RVecLbl.BackColor = System.Drawing.Color.Transparent;
+            this.RVecLbl.Location = new System.Drawing.Point(208, 130);
+            this.RVecLbl.Name = "RVecLbl";
+            this.RVecLbl.Size = new System.Drawing.Size(0, 21);
+            this.RVecLbl.TabIndex = 15;
+            // 
+            // SumaBtn
+            // 
+            this.SumaBtn.Location = new System.Drawing.Point(53, 191);
+            this.SumaBtn.Name = "SumaBtn";
+            this.SumaBtn.Size = new System.Drawing.Size(90, 33);
+            this.SumaBtn.TabIndex = 16;
+            this.SumaBtn.Text = "Suma";
+            this.SumaBtn.UseVisualStyleBackColor = true;
+            this.SumaBtn.Click += new System.EventHandler(this.SumaBtn_Click);
+            // 
+            // RestaBtn
+            // 
+            this.RestaBtn.Location = new System.Drawing.Point(149, 191);
+            this.RestaBtn.Name = "RestaBtn";
+            this.RestaBtn.Size = new System.Drawing.Size(90, 33);
+            this.RestaBtn.TabIndex = 17;
+            this.RestaBtn.Text = "Resta";
+            this.RestaBtn.UseVisualStyleBackColor = true;
+            this.RestaBtn.Click += new System.EventHandler(this.RestaBtn_Click);
+            // 
+            // MultiplicacionBtn
+            // 
+            this.MultiplicacionBtn.Location = new System.Drawing.Point(53, 230);
+            this.MultiplicacionBtn.Name = "MultiplicacionBtn";
+            this.MultiplicacionBtn.Size = new System.Drawing.Size(137, 33);
+            this.MultiplicacionBtn.TabIndex = 18;
+            this.MultiplicacionBtn.Text = "Multiplicacion";
+            this.MultiplicacionBtn.UseVisualStyleBackColor = true;
+            this.MultiplicacionBtn.Click += new System.EventHandler(this.MultiplicacionBtn_Click);
+            // 
+            // OtroBtn
+            // 
+            this.OtroBtn.Location = new System.Drawing.Point(196, 230);
+            this.OtroBtn.Name = "OtroBtn";
+            this.OtroBtn.Size = new System.Drawing.Size(90, 33);
+            this.OtroBtn.TabIndex = 19;
+            this.OtroBtn.Text = "Otro";
+            this.OtroBtn.UseVisualStyleBackColor = true;
+            this.OtroBtn.Click += new System.EventHandler(this.OtroBtn_Click);
+            // 
+            // SalirBtn
+            // 
+            this.SalirBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.SalirBtn.Location = new System.Drawing.Point(528, 289);
+            this.SalirBtn.Name = "SalirBtn";
+            this.SalirBtn.Size = new System.Drawing.Size(83, 32);
+            this.SalirBtn.TabIndex = 20;
+            this.SalirBtn.Text = "Salir";
+            this.SalirBtn.UseVisualStyleBackColor = true;
+            this.SalirBtn.Click += new System.EventHandler(this.SalirBtn_Click);
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.CancelButton = this.SalirBtn;
+            this.ClientSize = new System.Drawing.Size(623, 333);
+            this.Controls.Add(this.SalirBtn);
+            this.Controls.Add(this.OtroBtn);
+            this.Controls.Add(this.MultiplicacionBtn);
+            this.Controls.Add(this.RestaBtn);
+            this.Controls.Add(this.SumaBtn);
+            this.Controls.Add(this.RVecLbl);
+            this.Controls.Add(this.VecRLbl);
+            this.Controls.Add(this.KKLbl);
+            this.Controls.Add(this.JJLbl);
+            this.Controls.Add(this.IILbl);
+            this.Controls.Add(this.KLbl);
+            this.Controls.Add(this.JLbl);
+            this.Controls.Add(this.ILbl);
+            this.Controls.Add(this.KKTxt);
+            this.Controls.Add(this.JJTxt);
+            this.Controls.Add(this.IITxt);
+            this.Controls.Add(this.KTxt);
+            this.Controls.Add(this.JTxt);
+            this.Controls.Add(this.ITxt);
+            this.Controls.Add(this.VectorBLbl);
+            this.Controls.Add(this.VectorALbl);
+            this.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Calculadora";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label VectorALbl;
+        private System.Windows.Forms.Label VectorBLbl;
+        private System.Windows.Forms.TextBox ITxt;
+        private System.Windows.Forms.TextBox JTxt;
+        private System.Windows.Forms.TextBox KTxt;
+        private System.Windows.Forms.TextBox IITxt;
+        private System.Windows.Forms.TextBox JJTxt;
+        private System.Windows.Forms.TextBox KKTxt;
+        private System.Windows.Forms.Label ILbl;
+        private System.Windows.Forms.Label JLbl;
+        private System.Windows.Forms.Label KLbl;
+        private System.Windows.Forms.Label IILbl;
+        private System.Windows.Forms.Label JJLbl;
+        private System.Windows.Forms.Label KKLbl;
+        private System.Windows.Forms.Label VecRLbl;
+        private System.Windows.Forms.Label RVecLbl;
+        private System.Windows.Forms.Button SumaBtn;
+        private System.Windows.Forms.Button RestaBtn;
+        private System.Windows.Forms.Button MultiplicacionBtn;
+        private System.Windows.Forms.Button OtroBtn;
+        private System.Windows.Forms.Button SalirBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculdoraVectorial
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        double i, j, k, ii, jj, kk;
+
+        private void MultiplicacionBtn_Click(object sender, EventArgs e)
+        {
+            ITxt.Clear();
+            JTxt.Clear();
+            KTxt.Clear();
+            IITxt.Clear();
+            JJTxt.Clear();
+            KKTxt.Clear();
+
+            
+
+            RVecLbl.Text = "";
+        }
+
+        private void SalirBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void OtroBtn_Click(object sender, EventArgs e)
+        {
+           ITxt.Clear();
+           JTxt.Clear();
+           KTxt.Clear();
+           IITxt.Clear();
+           JJTxt.Clear();
+           KKTxt.Clear();
+
+           RVecLbl.Text = "";
+        }
+
+        private void RestaBtn_Click(object sender, EventArgs e)
+        {
+
+            double Restai;
+            double Restaj;
+            double Restak;
+
+            i = Convert.ToDouble(ITxt.Text);
+            j = Convert.ToDouble(JTxt.Text);
+            k = Convert.ToDouble(KTxt.Text);
+            ii = Convert.ToDouble(IITxt.Text);
+            jj = Convert.ToDouble(JJTxt.Text);
+            kk = Convert.ToDouble(KKTxt.Text);
+
+            Restai = i - ii;
+            Restaj = j - jj;
+            Restak = k - kk;
+
+            RVecLbl.Text = $" La suma de vectores fue: " + Restai + " i " + Restaj + " j " + Restak + " k ";
+
+        }
+
+
+
+        private void SumaBtn_Click(object sender, EventArgs e)
+        {
+
+            double Sumai;
+            double Sumaj;
+            double Sumak;
+
+            i = Convert.ToDouble(ITxt.Text);
+            j = Convert.ToDouble(JTxt.Text);
+            k = Convert.ToDouble(KTxt.Text);
+            ii = Convert.ToDouble(IITxt.Text);
+            jj = Convert.ToDouble(JJTxt.Text);
+            kk = Convert.ToDouble(KKTxt.Text);
+
+            Sumai = i + ii;
+            Sumaj = j + jj;
+            Sumak = k + kk;
+
+            RVecLbl.Text=$" La suma de vectores fue: " +Sumai + " i " +Sumaj + " j " +Sumak + " k ";
+
+        }
+    }
+}
